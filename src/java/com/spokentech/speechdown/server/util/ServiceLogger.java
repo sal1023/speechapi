4f5b29f45d3bf3af921d7bfeda632196989e933c
slord <sal1023@users.noreply.github.com>
2010-05-02 21:56:12 +0000

    try/catch/finally for hibernate

--- src/java/com/spokentech/speechdown/server/util/ServiceLogger.java
+++ src/java/com/spokentech/speechdown/server/util/ServiceLogger.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 import com.spokentech.speechdown.server.domain.HttpRequest;
 import com.spokentech.speechdown.server.domain.RecogRequest;
@@ -14,33 +15,64 @@ import com.spokentech.speechdown.server.domain.SynthRequest;
 
 public class ServiceLogger {
 
-	public static void logHttpRequest(HttpRequest hr) {
-        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
-        session.beginTransaction();
-        session.save(hr);
-        session.getTransaction().commit();
+	public static void logHttpRequest(HttpRequest hr) throws Exception {
+		
+	     Transaction transaction = null;
+	     Session session = null;
+	     try {
+	         session = HibernateUtil.getSessionFactory().getCurrentSession();
+	         transaction = session.beginTransaction();
+	         session.save(hr);
+	         session.getTransaction().commit();
+	      } catch (Exception e) { 
+	           if (transaction != null) {
+	             transaction.rollback();
+	             throw e;
+	           }
+	      }  finally { 
+	           session.close();
+	      }
     }
 	
 	
 	
   
-	public static void logRecogRequest(RecogRequest rr,HttpRequest hr) {
-
-		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
-        session.beginTransaction();
-           
-        session.save(rr);
-        session.getTransaction().commit();
+	public static void logRecogRequest(RecogRequest rr,HttpRequest hr) throws Exception {
+		
+	     Transaction transaction = null;
+	     Session session = null;
+	     try {
+	         session = HibernateUtil.getSessionFactory().getCurrentSession();
+	         transaction = session.beginTransaction();
+	         session.save(rr);
+	         session.getTransaction().commit();
+	      } catch (Exception e) { 
+	           if (transaction != null) {
+	             transaction.rollback();
+	             throw e;
+	           }
+	      }  finally { 
+	           session.close();
+	      }	
     }
 	
-	public static void logSynthRequest(SynthRequest sr,HttpRequest hr) {
-
-		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
-        session.beginTransaction();
-        
-   
-        session.save(sr);
-        session.getTransaction().commit();
+	public static void logSynthRequest(SynthRequest sr,HttpRequest hr) throws Exception {
+		
+	     Transaction transaction = null;
+	     Session session = null;
+	     try {
+	         session = HibernateUtil.getSessionFactory().getCurrentSession();
+	         transaction = session.beginTransaction();
+	         session.save(sr);
+	         session.getTransaction().commit();
+	      } catch (Exception e) { 
+	           if (transaction != null) {
+	             transaction.rollback();
+	             throw e;
+	           }
+	      }  finally { 
+	           session.close();
+	      }
     }
 
 	
